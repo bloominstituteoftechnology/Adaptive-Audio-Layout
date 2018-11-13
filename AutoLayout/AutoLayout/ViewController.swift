6852876cd983422b6e6f9881e97bd6144c1c5cf1
Frulwinn <frulwinn@gmail.com>
2018-11-13 15:13:36 -0800

    added outlets and actions to the play and pause button

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -2,6 +2,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var play: UIBarButtonItem!
     
+    @IBOutlet weak var pause: UIBarButtonItem!
+    
+    @IBOutlet var player: AudioHelper!
+
+    @IBAction func play(_ sender: Any) {
+        play.isEnabled = false
+        pause.isEnabled = true
+        player.play()
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        play.isEnabled = true
+        pause.isEnabled = false
+        player.pause()
+    }
 }
 
