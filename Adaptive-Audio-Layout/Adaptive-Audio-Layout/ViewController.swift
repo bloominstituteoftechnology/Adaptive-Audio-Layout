ae85197604c9f1a84404bf04faabde1f8781b3d7
Ben Hakes <bhhakes@gmail.com>
2018-11-13 16:56:33 -0600

    clense stock code

--- Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
+++ Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  Adaptive-Audio-Layout
-//
-//  Created by Benjamin Hakes on 11/13/18.
-//  Copyright © 2018 Benjamin Hakes. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
 
 }
