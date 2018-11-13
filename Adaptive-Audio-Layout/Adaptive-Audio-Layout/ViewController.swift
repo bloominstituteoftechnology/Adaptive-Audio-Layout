56a20517fd8a914efa382a0a473134bfb6b5b06c
AustinBCole <austin.cole.chileno@gmail.com>
2018-11-13 16:49:54 -0700

    Add initial functionality.

---
+++ Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var playButton: UIBarButtonItem!
+    
+    
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
+    
+    
+    @IBOutlet var player: AudioHelper!
+    
+    
+    @IBAction func play(_ sender: Any) {
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        player.play()
+    }
+    
+    
+    @IBAction func pause(_ sender: Any) {
+        playButton.isEnabled = true
+        pauseButton.isEnabled = false
+        player.pause()
+    }
+    
+    
+    
+}
+
