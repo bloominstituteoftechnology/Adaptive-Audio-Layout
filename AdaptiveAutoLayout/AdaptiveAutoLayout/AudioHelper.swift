fb474417751e5ec24b0055722fbb972a85b3133b
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-11-13 17:23:48 -0500

    add a new audio player file

---
+++ AdaptiveAutoLayout/AdaptiveAutoLayout/AudioHelper.swift
@@ -0,0 +1,9 @@
+//
+//  AudioHelper.swift
+//  AdaptiveAutoLayout
+//
+//  Created by Julian A. Fordyce on 11/13/18.
+//  Copyright © 2018 Julian A. Fordyce. All rights reserved.
+//
+
+import Foundation
