1c37c4717c9afb231dba79925f3a597617215169
Sergey Osipyan <osipyansergey@gmail.com>
2018-11-13 15:23:11 -0800

    App build but ther is layout problems

--- Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
+++ Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
@@ -3,9 +3,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-}
+    }
+    @IBOutlet weak var playButton: UIBarButtonItem!
+    
 
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
+    
+    @IBOutlet var player: AudioHelper!
+    
+    
+    @IBAction func playButton(_ sender: Any) {
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        player.play()
+        
+    }
+    
+    @IBAction func pauseButton(_ sender: Any) {
+        playButton.isEnabled = true
+        pauseButton.isEnabled = false
+        player.pause()
+        
+    }
+}
