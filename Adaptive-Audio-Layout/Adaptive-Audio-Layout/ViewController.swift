3c74d0fc0c87ad1b899051925e1609e4a93f8128
jkaunert <jkaunert@clusterfly.net>
2018-11-13 23:13:55 -0600

    adds all functionality for Minimum Viable Product

--- Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
+++ Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
@@ -3,5 +3,21 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var playButton: UIBarButtonItem!
+    
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
+    
+    @IBAction func play(_ sender: Any) {
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        player.play()
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        playButton.isEnabled = true
+        pauseButton.isEnabled = false
+        player.pause()
+    }
+    @IBOutlet var player: AudioHelper!
 }
 
