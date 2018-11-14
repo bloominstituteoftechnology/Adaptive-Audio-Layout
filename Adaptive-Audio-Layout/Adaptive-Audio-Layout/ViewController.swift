0fdae4474f8a3aac39817bb134775283bebc6a6f
Ben Hakes <bhhakes@gmail.com>
2018-11-13 21:15:56 -0600

    add toolbar with IBOutlets and IBact

--- Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
+++ Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
@@ -4,6 +4,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    @IBOutlet weak var playButton: UIToolbar!
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
+    
+    
+    
+    @IBAction func pause(_ sender: Any) {
+        
+    }
+    @IBAction func play(_ sender: Any) {
+        
+    }
 }
 
