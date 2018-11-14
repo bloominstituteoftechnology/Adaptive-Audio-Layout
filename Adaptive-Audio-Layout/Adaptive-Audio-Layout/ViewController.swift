9e683267ae9bc8a76ecad438a1ae03faf3db87de
Ben Hakes <bhhakes@gmail.com>
2018-11-13 21:20:02 -0600

    add Audio Helper and link into View Controller

--- Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
+++ Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
@@ -4,16 +4,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var playButton: UIToolbar!
+    @IBOutlet weak var playButton: UIBarButtonItem!
     @IBOutlet weak var pauseButton: UIBarButtonItem!
-    
+    @IBOutlet weak var player: AudioHelper!
     
     
     @IBAction func pause(_ sender: Any) {
-        
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        player.play()
     }
     @IBAction func play(_ sender: Any) {
-        
+        playButton.isEnabled = true
+        pauseButton.isEnabled = false
+        player.pause()
     }
 }
 
