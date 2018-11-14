aaef1a8b052992019fbd23a16109844d8a78612d
Jonah Bergevin <jonahbergevin@gmail.com>
2018-11-13 18:04:20 -0700

    Add Now Playing label that only appears while music is playing.

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var playButton: UIBarButtonItem!
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
+    @IBOutlet weak var nowPlayingLabel: UILabel!
+    
+    
+    @IBOutlet var player: AudioHelper!
+    
+    
+    @IBAction func play(_ sender: Any) {
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        player.play()
+        
+        if playButton.isEnabled == false {
+            nowPlayingLabel.text = "Now Playing"
+            print(nowPlayingLabel.text)
+        }
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        playButton.isEnabled = true
+        pauseButton.isEnabled = false
+        player.pause()
+        
+        if playButton.isEnabled == true {
+            nowPlayingLabel.text = ""
+            print(nowPlayingLabel.text)
+        }
+    }
+}
+
