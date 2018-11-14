d7d6c06f3179fa1425569f92d230e4a13e7de82c
Ben Hakes <bhhakes@gmail.com>
2018-11-13 22:18:31 -0600

    swap functions of play and pause (bug fix)

--- Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
+++ Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
@@ -10,14 +10,14 @@ class ViewController: UIViewController {
     
     
     @IBAction func pause(_ sender: Any) {
-        playButton.isEnabled = false
-        pauseButton.isEnabled = true
-        player.play()
-    }
-    @IBAction func play(_ sender: Any) {
         playButton.isEnabled = true
         pauseButton.isEnabled = false
         player.pause()
+    }
+    @IBAction func play(_ sender: Any) {
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        player.play()
     }
 }
 
