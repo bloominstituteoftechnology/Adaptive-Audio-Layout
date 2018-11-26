2305ee1849cb304800944e2d608dad0e3a45edb6
Meera Andersen <meera.andersen@gmail.com>
2018-11-26 15:09:01 -0700

    Add toolbar and buttons

---
+++ Auto Layout 1 Project/Auto Layout 1 Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout 1 Project
+//
+//  Created by Meera Andersen on 11/26/18.
+//  Copyright © 2018 Meera Andersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
