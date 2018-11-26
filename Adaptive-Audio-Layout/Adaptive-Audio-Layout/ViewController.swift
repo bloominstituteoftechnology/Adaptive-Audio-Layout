90bad9958e0bc1946837833774d95da1fc92356a
Vijay Das <vijaytdas@gmail.com>
2018-11-26 17:43:36 -0600

    added functionality with outlets and actions. added autolayount constraints and then deleted to rretry

--- Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
+++ Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var player: AudioHelper!
+    
+    @IBOutlet weak var playButton: UIBarButtonItem!
+    
+    
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func play(_ sender: Any) {
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        player.play()
+        
+    }
+ 
+    
+    @IBAction func pause(_ sender: Any) {
+        playButton.isEnabled = true
+        pauseButton.isEnabled = false
+        player.pause()
+        
+        
+    }
+    
+    
 }
 
