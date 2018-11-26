d5bfbcc31839a95dfc93e4afb147e42167213132
Sameera Leola <sameeraleola@gmail.com>
2018-11-26 15:48:26 -0600

    Initial commit

---
+++ AdaptiveAutoLayout/AdaptiveAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveAutoLayout
+//
+//  Created by Sameera Leola on 11/26/18.
+//  Copyright © 2018 Sameera Leola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
