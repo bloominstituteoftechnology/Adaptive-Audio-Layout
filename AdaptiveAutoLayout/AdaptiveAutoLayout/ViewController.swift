8023a982499f838244bc26442c3e8f465e71a29a
Dillon McElhinney <dillon.mce@gmail.com>
2018-11-26 16:12:51 -0700

    Get main storyboard laid out.

---
+++ AdaptiveAutoLayout/AdaptiveAutoLayout/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AdaptiveAutoLayout
+//
+//  Created by Dillon McElhinney on 11/26/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var playButton: UIBarButtonItem!
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
+    @IBOutlet var audioHelper: AudioHelper!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
