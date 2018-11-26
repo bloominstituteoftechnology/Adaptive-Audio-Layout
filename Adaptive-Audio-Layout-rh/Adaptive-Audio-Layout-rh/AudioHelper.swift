0f1066810f9016aa01f263f0d4a8aea4ed49492b
Rob Herold <rderek@bresnan.net>
2018-11-26 15:48:03 -0700

    initial commit; basic vertical layout

---
+++ Adaptive-Audio-Layout-rh/Adaptive-Audio-Layout-rh/AudioHelper.swift
@@ -0,0 +1,9 @@
+//
+//  AudioHelper.swift
+//  Adaptive-Audio-Layout-rh
+//
+//  Created by Rob Herold on 11/26/18.
+//  Copyright © 2018 Rob Herold. All rights reserved.
+//
+
+import Foundation
---
+++ Adaptive-Audio-Layout-rh/Adaptive-Audio-Layout-rh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Adaptive-Audio-Layout-rh
+//
+//  Created by Rob Herold on 11/26/18.
+//  Copyright © 2018 Rob Herold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
