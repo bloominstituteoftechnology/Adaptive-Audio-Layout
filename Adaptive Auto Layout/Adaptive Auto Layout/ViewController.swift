a497428d1e0268f38e2a33bd19a39441e45dbb64
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2018-11-26 16:41:31 -0600

    Project Setup and IB Layout

---
+++ Adaptive Auto Layout/Adaptive Auto Layout/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Adaptive Auto Layout
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
