e0bbfcefbdd8e7e5b09847ebc2f54203187eb613
Meera Andersen <meera.andersen@gmail.com>
2018-11-26 17:04:36 -0700

    Add actions and outlets for buttons

--- Auto Layout 1 Project/Auto Layout 1 Project/ViewController.swift
+++ Auto Layout 1 Project/Auto Layout 1 Project/ViewController.swift
@@ -1,18 +1,25 @@
-//
-//  ViewController.swift
-//  Auto Layout 1 Project
-//
-//  Created by Meera Andersen on 11/26/18.
-//  Copyright © 2018 Meera Andersen. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playButton: UIBarButtonItem!
+    
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
+    
+    @IBAction func play(_ sender: Any) {
+        playButton.isEnabled = false
+        pauseButton.isEnabled = true
+        player.play()
+        
+    }
+    @IBAction func pause(_ sender: Any) {
+        playButton.isEnabled = true
+        pauseButton.isEnabled = false
+        player.pause()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
