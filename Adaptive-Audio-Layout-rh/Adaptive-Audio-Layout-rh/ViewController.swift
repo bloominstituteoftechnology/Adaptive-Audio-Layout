96238daa219a266199f744869b4e6b0927284e01
Rob Herold <rderek@bresnan.net>
2018-11-26 22:05:22 -0700

    fixed outlets and actions

--- Adaptive-Audio-Layout-rh/Adaptive-Audio-Layout-rh/ViewController.swift
+++ Adaptive-Audio-Layout-rh/Adaptive-Audio-Layout-rh/ViewController.swift
@@ -10,20 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var playButton: UIView!
+
+
     
+    @IBOutlet weak var playButton: UIBarButtonItem!
     
-    @IBOutlet var pauseButton: UIView!
-    
+    @IBOutlet weak var pauseButton: UIBarButtonItem!
     
     @IBOutlet var player: AudioHelper!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     @IBAction func play(_ sender: Any) {
         playButton.isEnabled = false
         pauseButton.isEnabled = true
