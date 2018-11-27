cb9de0c2e1f1e184e894a2739c80b0a820216f95
Sean Doyle <sdoylelambda@gmail.com>
2018-11-27 10:49:05 -0500

    cleaned up code

--- Audio/AppDelegate.swift
+++ Audio/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Audio
-//
-//  Created by Lambda_School_Loaner_18 on 10/10/18.
-//  Copyright © 2018 Lambda_School_Loaner_18. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
