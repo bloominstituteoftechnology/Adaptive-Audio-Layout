b069129eb1f4f1f594abbd65906402cd2d69270c
Sameera Leola <sameeraleola@gmail.com>
2018-12-03 17:14:26 -0600

    Completed IB horizontal and vertical layouts successfully

---
+++ Adaptive-Audio-Layout/Adaptive-Audio-Layout/AudioHelper.swift
@@ -0,0 +1,37 @@
+//
+//  AudioHelper.swift
+//  Adaptive-Audio-Layout
+//
+//  Created by Sameera Leola on 12/3/18.
+//  Sprint II Review Monday Project
+//
+
+import AVKit
+
+class AudioHelper: NSObject {
+    let player: AVPlayer
+    
+    override init() {
+        // Big Buck Bunny
+        let streamrequest = "https://www.radiantmediaplayer.com/media/bbb-360p.mp4"
+        guard let url = URL(string:streamrequest) else {
+            fatalError("Bad URL configuration")
+        }
+        let asset = AVAsset(url: url)
+        let item = AVPlayerItem(asset: asset)
+        
+        // Create a player member
+        self.player = AVPlayer(playerItem: item)
+        
+        // Init superclass members
+        super.init()
+    }
+    
+    func play() {
+        player.play()
+    }
+    
+    func pause() {
+        player.pause()
+    }
+}
