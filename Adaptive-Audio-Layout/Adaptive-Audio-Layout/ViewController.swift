03ca4fc561e58cab11be164dcf3dff28333e2ae1
Vijay Das <vijaytdas@gmail.com>
2018-12-03 14:44:20 -0600

    add Questions text file. add project file.

---
+++ Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Adaptive-Audio-Layout
+//
+//  Created by Vijay Das on 12/3/18.
+//  Copyright © 2018 Vijay Das. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
