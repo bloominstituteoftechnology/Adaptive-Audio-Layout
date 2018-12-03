b9ebb7ee0c8e7f933fbd20b8299759b193007a56
Sameera Leola <sameeraleola@gmail.com>
2018-12-03 13:32:31 -0600

    Created project

---
+++ Adaptive-Audio-Layout/Adaptive-Audio-Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Adaptive-Audio-Layout
+//
+//  Created by Sameera Leola on 12/3/18.
+//  Copyright © 2018 Sameera Leola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
